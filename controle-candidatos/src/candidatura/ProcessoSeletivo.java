418f4bd7b6ddc430d37a147249306aeb934cccad
ppablolds <pablosilvaaluno@outlook.com>
2025-03-11 18:34:44 -0300

    feat: Criando função Seleção de candidatos

--- controle-candidatos/src/candidatura/ProcessoSeletivo.java
+++ controle-candidatos/src/candidatura/ProcessoSeletivo.java
@@ -1,13 +1,39 @@
 package candidatura;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 public class ProcessoSeletivo {
     public static void main(String[] args) {
         System.out.println("Processo Seletivo!");
+        selecaoCandidatos();
 
-        analisarCandidato(1900.0);
-        analisarCandidato(2200.0);
-        analisarCandidato(2000.0);
+        // analisarCandidato(1900.0);
+        // analisarCandidato(2200.0);
+        // analisarCandidato(2000.0);
     }
+
+    static void selecaoCandidatos() {
+        String [] candidatos = {"FELIPE", "MARCIA", "JULIA", "PAULO", "AUGUSTO", "MONICA", "FABRICIO", "MIRELA", "DANIELA", "JORGE"};
+
+        int candidatosSelecionado = 0;
+        int candidatosAtual = 0;
+        double salarioBase = 2000.0;
+        while (candidatosSelecionado < 5) {
+            String candidato = candidatos[candidatosAtual];
+            double salarioPretendido = valorPretendido();
+
+            System.out.println("O candidato " + candidato + " solicitou esta valor de salário " + salarioPretendido);
+            if (salarioBase >= salarioPretendido && candidatosAtual < candidatos.length) {
+                System.out.println("O candidato " + candidato + " foi selecionado para a vaga!");
+                candidatosSelecionado++;
+            }
+            candidatosAtual++;
+        }
+    }
+    static double valorPretendido() {
+        return ThreadLocalRandom.current().nextDouble(1800, 2200);
+    }
+
     static void analisarCandidato(double salarioPretendido) {
         double salarioBase = 2000.0;
 
